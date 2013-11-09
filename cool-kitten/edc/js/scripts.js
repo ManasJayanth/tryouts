af52b38a27251b03862dd08da0340a3beecf6d1f
Manas <harlequinsguitar@gmail.com>
2013-11-10 04:18:09 +0530

    Temporary fix for invalid link selection in the navbar

--- cool-kitten/edc/js/scripts.js
+++ cool-kitten/edc/js/scripts.js
@@ -15,7 +15,21 @@ jQuery(document).ready(function ($) {
         dataslide = $(this).attr('data-slide');
 
         if (direction === 'down') {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+            $('.navigation li[data-slide="' + (parseInt(dataslide) + 1) + '"]').addClass('active').prev().removeClass('active');
+
+            /*** 
+            
+            4:11 am IST. November 9, 2013
+            Returning wrong slide on its way downwards - slide previous to the correct slide. Probably a bug in waypoint itself:
+
+                Temporary fix: 
+                $('.navigation li[data-slide="' + (parseInt(dataslide) + 1) + '"]').addClass('active').prev().removeClass('active');
+
+                Relevant link: https://github.com/imakewebthings/jquery-waypoints/pull/197
+
+            Update version of waypoint
+
+                ***/
         }
         else {
             $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
